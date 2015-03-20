8b95f182c109cc7849632d521ebda3544b7b93b8
ruttyj <ruttyj92@hotmail.com>
2015-03-20 16:27:18 -0400

    ThirdParty2013

---
+++ 371ProjectTeam4/Source/Models/GroupModel.h
@@ -0,0 +1,47 @@
+//
+// COMP 371 Assignment Framework
+//
+// Created by Nicolas Bergeron on 8/7/14.
+// Updated by Gary Chang on 14/1/15
+//
+// Copyright (c) 2014-2015 Concordia University. All rights reserved.
+//
+
+#pragma once
+//#include "HashTable.h"
+#include "Model.h"
+#include "../Path.h"
+#include "../BSpline.h"
+#include "../light.h"
+//#include "HashTable.h"
+
+class GroupModel : public Model {
+public:
+	GroupModel(glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f));
+	virtual ~GroupModel();
+	virtual void Update(float dt);
+	virtual void Draw();
+
+	virtual void SetRotationSpeed(float s);
+
+protected:
+	virtual bool ParseLine(const std::vector<ci_string> &token);
+
+private:
+
+	//Model vars
+	// The vertex format could be different for different types of models
+	struct Vertex {
+		glm::vec3 position;
+		glm::vec3 normal;
+		glm::vec3 color;
+	};
+	unsigned int mVertexArrayID;
+	unsigned int mVertexBufferID;
+	float rotationSpeed;
+
+
+	std::vector<Path*> mPath;
+    std::vector<BSpline*> mSpline;
+	std::vector<Light*> mLight;
+};
---
+++ 371ProjectTeam4/Source/light.h
@@ -0,0 +1,40 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+#define LIGHT_NULL -1
+#define LIGHT_DIRECTIONAL 0
+#define LIGHT_POINT 1
+
+#include "math2.h"
+#include <GLM/glm.hpp>
+
+class Light
+{
+public:
+	Light(glm::vec3 position, glm::vec3 direction, glm::vec3 color, glm::vec3 attenuation, float intensity);
+	Light(glm::vec3 position, glm::vec3 color, glm::vec3 attenuation, float intensity);
+	Light(glm::vec3 position, glm::vec3 color);
+	Light();
+
+	int getType()
+	{
+		if (m_direction != glm::vec3())
+			return LIGHT_DIRECTIONAL;
+
+		return LIGHT_POINT;
+	}
+
+	glm::vec3 getPosition();
+	glm::vec3 getDirection();
+	glm::vec3 getColor();
+	glm::vec3 getAttenuation();
+	float getIntensity();
+private:
+	glm::vec3 m_position;
+	glm::vec3 m_direction;
+	glm::vec3 m_color;
+	glm::vec3 m_attenuation;
+	float m_intensity;
+};
+
+#endif
