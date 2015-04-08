4b3709183fdde3e0c291806ef15c043a993e7393
provencher <eprovencher92@gmail.com>
2015-04-08 17:02:26 -0400

    bug fixes + cleanup

---
+++ 371ProjectTeam4/Source/Models/Terrain.h
@@ -0,0 +1,41 @@
+//
+// COMP 371 Assignment Framework
+//
+// Created by Nicolas Bergeron on 8/7/14.
+// Updated by Gary Chang on 14/1/15
+// Updated by Rita Phommarath on 2015/04/03
+// Copyright (c) 2014-2015 Concordia University. All rights reserved.
+//
+
+#pragma once
+
+#include "Model.h"
+
+class Terrain : public Model
+{
+public:
+	//events
+	Terrain(glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f));
+	virtual ~Terrain();
+	virtual void Update(float dt);
+	virtual void Draw();
+
+
+protected:
+	virtual bool ParseLine(const std::vector<ci_string> &token);
+
+private:
+	// The vertex format could be different for different types of models
+	struct Vertex
+	{
+		glm::vec3 position;
+		glm::vec3 normal;
+		glm::vec3 color;
+	};
+	GLuint Texture;
+	unsigned int mVertexArrayID;
+	unsigned int mVertexBufferID;
+	unsigned int uvbuffer;
+
+	
+};
